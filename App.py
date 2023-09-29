2f482f09382bc3a11406e449a71107f379f1feab
amirprync <60889238+amirprync@users.noreply.github.com>
2023-09-29 16:56:35 -0300

    Update App.py

--- App.py
+++ App.py
@@ -1,28 +1,23 @@
 import streamlit as st
 import pandas as pd
-import numpy as np
+from io import BytesIO
 import openpyxl
 from openpyxl.styles import PatternFill
 
 def highlight_rows(df):
-    red_fill = PatternFill(start_color="FFFF00",
-                           end_color="FFFF00", fill_type="solid")
-    
-    # Crear una copia del DataFrame para evitar modificar el DataFrame original
-    df_highlighted = df.copy()
-    
-    # Abrir el archivo Excel y obtener la hoja activa
-    with pd.ExcelWriter("highlighted_data.xlsx", engine='openpyxl') as writer:
+    output = BytesIO()
+    with pd.ExcelWriter(output, engine='openpyxl') as writer:
         df.to_excel(writer, index=False)
         wb = writer.book
         ws = wb.active
+        red_fill = PatternFill(start_color="FFFF00",
+                               end_color="FFFF00", fill_type="solid")
         
         for row in range(2, df.shape[0] + 2):
             comitente = ws.cell(row=row, column=1).value
             codigo_caja = ws.cell(row=row, column=2).value
             cantidad_a_transferir = ws.cell(row=row, column=5).value
             
-            # Comprobar si hay filas duplicadas según las condiciones
             condition = ((df['Comitente - Número'] == comitente) & 
                          (df['Instrumento - Código caja'] == codigo_caja) & 
                          (df['Transferencia - Cantidad a Transferir'] == cantidad_a_transferir))
@@ -30,8 +25,8 @@ def highlight_rows(df):
             if df[condition].shape[0] > 1:
                 for col in range(1, df.shape[1] + 1):
                     ws.cell(row=row, column=col).fill = red_fill
-
-        wb.save("highlighted_data.xlsx")
+    output.seek(0)
+    return output
 
 # Interfaz Streamlit
 st.title("Resaltador de Filas en Excel")
@@ -44,5 +39,10 @@ if uploaded_file is not None:
     st.write(df)
     
     if st.button('Resaltar Filas'):
-        highlight_rows(df)
-        st.success('Se ha creado un nuevo archivo Excel con las filas resaltadas.')
+        output = highlight_rows(df)
+        st.download_button(
+            label="Descargar archivo Excel resaltado",
+            data=output,
+            file_name='datos_resaltados.xlsx',
+            mime='application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'
+        )
