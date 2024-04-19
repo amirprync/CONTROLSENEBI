474248d2ec8803cb92355812071bbab2b5a7cd81
amirprync <60889238+amirprync@users.noreply.github.com>
2024-04-19 12:34:36 -0300

    Update App.py

--- App.py
+++ App.py
@@ -12,15 +12,15 @@ def compare_dataframes(df1, df2):
         raise ValueError("El segundo archivo no tiene las columnas esperadas.")
 
     # Renombrar las columnas para uniformidad
-    df1.rename(columns={'Comitente - Número': 'Numero', 'SENEBI - Precio de Referencia': 'Precio'}, inplace=True)
-    df2.rename(columns={'P.Número': 'Numero', 'Precio Productor/Comercial': 'Precio'}, inplace=True)
+    df1.rename(columns={'Comitente - Número': 'Numero', 'SENEBI - Precio de Referencia': 'Precio_1'}, inplace=True)
+    df2.rename(columns={'P.Número': 'Numero', 'Precio Productor/Comercial': 'Precio_2'}, inplace=True)
     
     # Unir los dataframes para comparar
-    merged_df = pd.merge(df1, df2, on='Numero', how='outer', suffixes=('_1', '_2'))
+    merged_df = pd.merge(df1, df2, on='Numero', how='outer')
     
-    # Comprobar si las columnas de precios están presentes
-    if 'Precio_1' not in merged_df or 'Precio_2' not in merged_df:
-        raise ValueError("Error en el proceso de fusión, revisa los nombres de columnas y los datos.")
+    # Rellenar valores faltantes para asegurar la alineación
+    merged_df['Precio_1'].fillna(value=pd.NA, inplace=True)
+    merged_df['Precio_2'].fillna(value=pd.NA, inplace=True)
 
     # Filtrar las filas donde los precios no coinciden
     discrepancies = merged_df[merged_df['Precio_1'] != merged_df['Precio_2']]
@@ -33,7 +33,6 @@ file1 = st.file_uploader("Subir archivo Excel 1", type=['xlsx'])
 file2 = st.file_uploader("Subir archivo Excel 2", type=['xlsx'])
 
 if file1 and file2:
-    # Cargar los datos
     df1 = load_data(file1)
     df2 = load_data(file2)
 
